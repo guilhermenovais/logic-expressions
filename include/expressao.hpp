4ec2a1569d7b33b1e68133f09a7c083ecb0bed0c
Guilherme Novais <guilhermenovaisdeosouza@proton.me>
2023-09-28 21:37:28 -0300

    feat: criando função que constrói árvore

--- include/expressao.hpp
+++ include/expressao.hpp
@@ -1,24 +1,29 @@
 #ifndef EXPRESSAO_HPP
 #define EXPRESSAO_HPP
 
-#include "infixa.hpp"
-#include "posfixa.hpp"
+#include "arvore_binaria.hpp"
 #include <string>
 
 class Expressao {
     private:
-        std::string str_expressao;
-        Posfixa posfixa;
-        Infixa infixa;
-        int tipo_exp;
+        ArvoreBinaria arvore_expressao;
+        bool valores[100];
+        std::string tipo;
+        // Posfixa posfixa;
+        // Infixa infixa;
+        // int tipo_exp;
     public:
-        bool lePosfixa(std::string expressao);
-        bool leInfixa(std::string expressao);
-        bool infParaPos();
-        bool posParaInf();
-        double resolve();
-        std::string getExp();
-        std::string getTipoExp();
+        Expressao(std::string expressao, std::string valores, std::string tipo);
+        void constroiArvore(std::string expressao);
+        void leValor(TipoNo** raiz, TipoNo** anterior, std::string expressao, int indice);
+        int getPrecedencia(char valor);
+        // bool lePosfixa(std::string expressao);
+        // bool leInfixa(std::string expressao);
+        // bool infParaPos();
+        // bool posParaInf();
+        // double resolve();
+        // std::string getExp();
+        // std::string getTipoExp();
 };
 
 #endif //EXPRESSAO_HPP
--- src/expressao.cpp
+++ src/expressao.cpp
@@ -3,61 +3,142 @@
 #include <iostream>
 #include <stdexcept>
 
-bool Expressao::lePosfixa(std::string expressao) {
-    tipo_exp = 0;
-    bool exp_valida = posfixa.leExpressao(expressao);
-    if(exp_valida) str_expressao = expressao;
-    return exp_valida;
+Expressao::Expressao(std::string expressao, std::string valores, std::string str_tipo) {
+    constroiArvore(expressao);
+    if(tipo == "-a") tipo = "avaliador" ;
+    else tipo =  "satisfabilidade";
 }
 
-bool Expressao::leInfixa(std::string expressao) {
-    tipo_exp = 1;
-    bool exp_valida = infixa.leExpressao(expressao);
-    if(exp_valida) str_expressao = expressao;
-    return exp_valida;
+void Expressao::constroiArvore(std::string expressao) {
+    TipoNo* raiz = nullptr;
+    leValor(&raiz, &raiz, expressao, 0);
+    arvore_expressao = ArvoreBinaria();
+    arvore_expressao.defineRaiz(raiz);
 }
 
-bool Expressao::infParaPos() {
-    if(str_expressao.empty()) return false;
+void Expressao::leValor(TipoNo** raiz, TipoNo** anterior, std::string expressao, int indice) {
+    // Pula espaços
+    while (indice < expressao.length()) {
+        if(expressao[indice] == ' ') indice++;
+        else break;
+    }
+
+    if(indice >= expressao.length()) {
+        return;
+    }
+
+    TipoNo* atual = new TipoNo(expressao[indice]);
+
+    if(*raiz == nullptr) *raiz = atual;
+    else {
+        TipoNo* candidato_a_pai = *anterior;
+        while(1) {
+            // Caso a precedência do pai seja menor
+            if(getPrecedencia(atual->valor) > getPrecedencia(candidato_a_pai->valor)) {
+                atual->pai = candidato_a_pai;
+                candidato_a_pai->dir = atual;
+                break;
+            // Caso a precedência do pai seja maior
+            } else {
+                // Caso o pai seja raiz
+                if(candidato_a_pai == *raiz) {
+                    atual->esq = candidato_a_pai;
+                    candidato_a_pai->pai = atual;
+                    *raiz = atual;
+                    break;
+                } else {
+                    // Caso a precedência do pai seja maior mas a precedência do avô seja menor
+                    if(getPrecedencia(atual->valor) > getPrecedencia(candidato_a_pai->pai->valor)) {
+                        atual->pai = candidato_a_pai->pai;
+                        candidato_a_pai->pai->dir = atual;
+                        atual->esq = candidato_a_pai;
+                        candidato_a_pai->pai = atual;
+                        break;
+                    // Caso o pai e o avô tenham precedência menor
+                    } else {
+                        candidato_a_pai = candidato_a_pai->pai;
+                    }
+                }
+            }
+        }
+    }
+
+    leValor(raiz, &atual, expressao, indice + 1);
+}
+
+int Expressao::getPrecedencia(char valor) {
+    switch(valor) {
+        case '(':
+            return 0;
+        case '|':
+            return 1;
+        case '&':
+            return 2;
+        case '0':
+        case '1':
+            return 3;
+        case ')':
+            return 4;
+        default:
+            return 4;
+    }
+}
+
+// bool Expressao::lePosfixa(std::string expressao) {
+//     tipo_exp = 0;
+//     bool exp_valida = posfixa.leExpressao(expressao);
+//     if(exp_valida) str_expressao = expressao;
+//     return exp_valida;
+// }
+
+// bool Expressao::leInfixa(std::string expressao) {
+//     tipo_exp = 1;
+//     bool exp_valida = infixa.leExpressao(expressao);
+//     if(exp_valida) str_expressao = expressao;
+//     return exp_valida;
+// }
+
+// bool Expressao::infParaPos() {
+//     if(str_expressao.empty()) return false;
     
-    try {
-        str_expressao = posfixa.infParaPos(str_expressao);
-        tipo_exp = 0;
-        return true;
-    } catch (const std::exception& e) {
-        return false;
-    }
-}
+//     try {
+//         str_expressao = posfixa.infParaPos(str_expressao);
+//         tipo_exp = 0;
+//         return true;
+//     } catch (const std::exception& e) {
+//         return false;
+//     }
+// }
 
-bool Expressao::posParaInf() {
-    if(str_expressao.empty()) return false;
-    try {
-        str_expressao = infixa.posParaInf(str_expressao);
-        tipo_exp = 1;
-        return true;
-    } catch (const std::exception& e) {
-        return false;
-    }
-}
+// bool Expressao::posParaInf() {
+//     if(str_expressao.empty()) return false;
+//     try {
+//         str_expressao = infixa.posParaInf(str_expressao);
+//         tipo_exp = 1;
+//         return true;
+//     } catch (const std::exception& e) {
+//         return false;
+//     }
+// }
 
-double Expressao::resolve() {
-    if(tipo_exp == 0) {
-        return posfixa.resolve();
-    } else if(tipo_exp == 1) {
-        return infixa.resolve();
-    }
+// double Expressao::resolve() {
+//     if(tipo_exp == 0) {
+//         return posfixa.resolve();
+//     } else if(tipo_exp == 1) {
+//         return infixa.resolve();
+//     }
 
-    throw std::domain_error("ERRO: EXP NAO EXISTE");
-}
+//     throw std::domain_error("ERRO: EXP NAO EXISTE");
+// }
 
-std::string Expressao::getExp() {
-    return str_expressao;
-}
+// std::string Expressao::getExp() {
+//     return str_expressao;
+// }
 
-std::string Expressao::getTipoExp() {
-    if(tipo_exp == 0) {
-        return "POSFIXA";
-    } else {
-        return "INFIXA";
-    }
-}
+// std::string Expressao::getTipoExp() {
+//     if(tipo_exp == 0) {
+//         return "POSFIXA";
+//     } else {
+//         return "INFIXA";
+//     }
+// }
